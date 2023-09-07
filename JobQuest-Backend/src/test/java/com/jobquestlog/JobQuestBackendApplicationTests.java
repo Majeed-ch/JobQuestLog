0f9e7af95bdc5c332d652144f547581822a3f5f9
AbdulMajeed <chowdhury4554@gmail.com>
2023-09-07 13:32:54 -0400

    Initial commit

---
+++ JobQuest-Backend/src/test/java/com/jobquestlog/JobQuestBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobquestlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobQuestBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
